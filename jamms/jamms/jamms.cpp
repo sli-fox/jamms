b25b3c45e091e12546b67d632457698702688794
unknown <stephanie.zhongsui.li@gmail.com>
2014-09-16 20:37:20 -0400

    Redo project creation

--- jamms/jamms/jamms.cpp
+++
@@ -1,7 +0,0 @@
-// jamms.cpp : Defines the entry point for the console application
-
-int main(int argc, char** argv) {
-	//Game::Start();
-
-	return 0;
-}
--- jamms/jamms/jamms.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class jamms
-{
-public:
-	jamms(void);
-	~jamms(void);
-};
-
