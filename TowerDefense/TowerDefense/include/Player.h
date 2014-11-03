8de5e6a95693c8533a22f72a65e31e9c4969502c
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2014-11-03 14:31:56 -0500

    Player class initial commit

--- TowerDefense/TowerDefense/include/Player.h
+++ TowerDefense/TowerDefense/include/Player.h
@@ -10,17 +10,22 @@ public:
 
 	std::string getName() const;
 	int getCash() const;
+	int getPoints() const;
 	int getHighScore() const;
+	int getLives() const;
 
 	void earnCash(int cash);
 	void spendCash(int cash);
 	void gainPoints(int points);
 	void losePoints(int points);
 	void setHighScore(int score);
+	void earnLives(int lives);
+	void loseLives(int lives);
 	
 private:
-	std::string name;
-	int cash; // utilized for buying towers
-	int points; // acquired during gameplay for defeating critters
-	int highScore;
+	std::string _name;
+	int _cash; // utilized for buying towers
+	int _points; // acquired during gameplay for defeating critters
+	int _high_score;
+	int _lives; // decrememnt when critters reach end of path
 };
--- TowerDefense/TowerDefense/src/Player.cpp
+++ TowerDefense/TowerDefense/src/Player.cpp
@@ -4,31 +4,70 @@ Player::Player() {
 
 }
 
-Player::Player(std::string name) : name(name){
-	
+Player::Player(std::string name) : _name(name){
+	this->_cash = 0;
+	this->_points = 0;
+	this->_high_score = 0;
+	this->_lives = 20;
 }
 
 Player::~Player() {
 
 }
 
+std::string Player::getName() const {
+	return this->_name;
+}
+
+int Player::getCash() const {
+	return this->_cash;
+}
+
+int Player::getHighScore() const {
+	return this->_high_score;
+}
+
+int Player::getLives() const {
+	return this->_lives;
+}
+
 void Player::earnCash(int cash) {
-	this->cash += cash;
+	this->_cash += cash;
 }
 
+/**
+  * @brief Maintains non-negative cash value
+  * @return void
+  */
 void Player::spendCash(int cash) {
-	this->cash = (this->cash - cash < 0 ? 0 : this->cash - cash);
-}
-
-void Player::setHighScore(int score) {
-	if(score > this->highScore)
-		this->highScore = score; 
+	this->_cash = (this->_cash - cash < 0 ? 0 : this->_cash - cash);
 }
 
 void Player::gainPoints(int points) {
-	this->points += points;
+	this->_points += points;
 }
 
+/**
+  * @brief Maintains non-negative points value
+  * @return void
+  */
 void Player::losePoints(int points) {
-	this->points = (this->points - points < 0 ? 0 : this->points - points);
+	this->_points = (this->_points - points < 0 ? 0 : this->_points - points);
+}
+
+/**
+  * @brief Sets new high score if greater than old one
+  * @return void
+  */
+void Player::setHighScore(int score) {
+	if(score > this->_high_score)
+		this->_high_score = score; 
+}
+
+void Player::earnLives(int lives) {
+	this->_lives += lives;
+}
+
+void Player::loseLives(int lives) {
+	this->_lives -= lives;
 }
