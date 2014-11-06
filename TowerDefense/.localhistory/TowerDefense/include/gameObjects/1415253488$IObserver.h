e147e42e4ce530dbe9097189ca16cd16765d8cbf
Mark Massoud <markmssd@gmail.com>
2014-11-06 03:30:28 -0500

    Towers attack fartest critter

--- TowerDefense/.localhistory/TowerDefense/include/gameObjects/1415253488$IObserver.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class IObserver {
-
-  public:
-    void update();
-
-};
--- TowerDefense/.localhistory/TowerDefense/include/gameObjects/1415260368$IObserver.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class IObserver {
-
-  public:
-    virtual void update() = 0;
-
-};
