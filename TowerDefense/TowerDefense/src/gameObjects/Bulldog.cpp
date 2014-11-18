d1bc4251bf8fc8286462a5d05515edbdd9094fcb
Mark Massoud <markmssd@gmail.com>
2014-11-18 12:35:02 -0500

    Range now uses Float instead of Enum

--- TowerDefense/TowerDefense/src/gameObjects/Bulldog.cpp
+++ TowerDefense/TowerDefense/src/gameObjects/Bulldog.cpp
@@ -4,21 +4,20 @@ int Bulldog::serial = 0;
 int const Bulldog::buy_cost = 70;
 
 Bulldog::Bulldog(int tileX, int tileY) {
+	this->load("resources/images/towers/bulldog_0.png");
+	this->setPosition(tileX*32, tileY*32);
+
 	this->_id = Bulldog::serial++;
 	this->_name = "Bulldog_" + std::to_string(this->_id);
 	this->_type = Tower::Bulldog;
 	this->_range_shape.setPosition((tileX*32)-16, (tileY*32)-16);
 	this->_buy_cost = buy_cost;
 	this->_power = 2;
-	this->_range = Tower::Range::Small;
-	this->setRangeShape(_range);
+	this->setRange(1);
 	this->_rate_of_fire = Tower::RateOfFire::Slow;
 	this->_special_effect = Tower::SpecialEffect::None;
 	this->_upgrade_cost = 20;
 	this->_sell_cost = 30;
-	
-	this->load("resources/images/towers/bulldog_0.png");
-	this->setPosition(tileX*32, tileY*32);
 	
 	std::cout << green << this->_name << " has been created." << std::endl;
 }
@@ -27,22 +26,20 @@ void Bulldog::upgradeTower() {
 	if(this->_upgrade_cost <= Game::player.getCash()) {
 		int oldUpgradeCost = this->_upgrade_cost;
 		if(this->getUpgradeLevel() == 0) {
-			this->setRangeShape(_range);
 			this->setUpgradeLevel(UpgradeLevel::Upgrade1);
 			this->load("resources/images/towers/bulldog_1.png");
 			this->setPower(3);
-			this->setRange(Tower::Range::Small);
+			this->setRange(1.5);
 			this->setRateOfFire(Tower::RateOfFire::Normal);
 			this->setSpecialEffect(Tower::SpecialEffect::None);
 			this->setUpgradeCost(20);
 			this->setSellCost(40);
 		}
 		else if(this->getUpgradeLevel() == 1) {
-			this->setRangeShape(_range);
 			this->setUpgradeLevel(UpgradeLevel::Upgrade2);
 			this->load("resources/images/towers/bulldog_2.png");
 			this->setPower(4);
-			this->setRange(Tower::Range::Small);
+			this->setRange(2);
 			this->setRateOfFire(Tower::RateOfFire::Normal);
 			this->setSpecialEffect(Tower::SpecialEffect::None);
 			this->setSellCost(50);
--- TowerDefense/TowerDefense/src/gameObjects/ShihTzu.cpp
+++ TowerDefense/TowerDefense/src/gameObjects/ShihTzu.cpp
@@ -4,21 +4,20 @@ int ShihTzu::serial = 0;
 int const ShihTzu::buy_cost = 50;
 
 ShihTzu::ShihTzu(int tileX, int tileY) {
+	this->load("resources/images/towers/ShihTzu_0.png");
+	this->setPosition(tileX*32, tileY*32);
+
 	this->_id = ShihTzu::serial++;
 	this->_name = "ShihTzu_" + std::to_string(this->_id);
 	this->_type = Tower::ShihTzu;
 	this->_range_shape.setPosition((tileX*32)-16, (tileY*32)-16);
 	this->_buy_cost = buy_cost;
 	this->_power = 1;
-	this->_range = Tower::Range::Small;
-	this->setRangeShape(_range);
+	this->setRange(1);
 	this->_rate_of_fire = Tower::RateOfFire::Normal;
 	this->_special_effect = Tower::SpecialEffect::None;
 	this->_upgrade_cost = 20;
 	this->_sell_cost = 20;
-
-	this->load("resources/images/towers/ShihTzu_0.png");
-	this->setPosition(tileX*32, tileY*32);
 	
 	std::cout << green << this->_name << " has been created." << std::endl;
 }
@@ -27,22 +26,20 @@ void ShihTzu::upgradeTower() {
 	if(this->_upgrade_cost <= Game::player.getCash()) {
 		int oldUpgradeCost = this->_upgrade_cost;
 		if(this->getUpgradeLevel() == 0) {
-			this->setRangeShape(_range);
 			this->setUpgradeLevel(UpgradeLevel::Upgrade1);
 			this->load("resources/images/towers/ShihTzu_1.png");
 			this->setPower(1);
-			this->setRange(Tower::Range::Medium);
+			this->setRange(1.5);
 			this->setRateOfFire(Tower::RateOfFire::Normal);
 			this->setSpecialEffect(Tower::SpecialEffect::None);
 			this->setUpgradeCost(30);
 			this->setSellCost(30);
 		}
 		else if(this->getUpgradeLevel() == 1) {
-			this->setRangeShape(_range);
 			this->setUpgradeLevel(UpgradeLevel::Upgrade2);
 			this->load("resources/images/towers/ShihTzu_2.png");
 			this->setPower(2);
-			this->setRange(Tower::Range::Large);
+			this->setRange(2);
 			this->setRateOfFire(Tower::RateOfFire::Normal);
 			this->setSpecialEffect(Tower::SpecialEffect::Slowing);
 			this->setSellCost(40);
