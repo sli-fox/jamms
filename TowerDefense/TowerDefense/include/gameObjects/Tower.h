403ccd799d0293aa5fddf5d1fde11627d29f91cc
unknown <stephanie.zhongsui.li@gmail.com>
2014-11-26 00:12:11 -0500

    Added class to decouple Tower from GUI

--- TowerDefense/TowerDefense/include/gameObjects/Tower.h
+++ TowerDefense/TowerDefense/include/gameObjects/Tower.h
@@ -1,18 +1,17 @@
 #pragma once
 #include <string>
-#include <GameObjects/GameObject.h>
+#include <GameObjects/TowerGameObject.h>
 #include <GameObjects/Critter.h>
 #include <GameObjects/IObserver.h>
 #include "Utils/TowerStrategy.h"
 #include <memory>
 
-class Tower: public GameObject, public IObserver{
+class Tower: public TowerGameObject, public IObserver{
 public:
   Tower();
   ~Tower() { std::cout << red << _name << " has been deleted." << white << std::endl; };
   void upgradeTower() {}
   virtual bool canAttack(Critter* critter) = 0;
-  bool canApplySpecialAfterEffects(Critter* critter);
   virtual void attack() = 0;
   void update();
   Critter* executeStrategy(Critter* critter);
@@ -22,7 +21,6 @@ public:
 
   enum TowerType { ShihTzu, Dalmatian, Bulldog };
   enum UpgradeLevel { Baby, Teen, Adult };
-  enum RateOfFire { Slow = 1, Normal, Fast };
   enum SpecialEffect { None, Slowing, Burning, Freezing };
 
   //ACCESSORS
@@ -32,8 +30,6 @@ public:
   Tower::UpgradeLevel getUpgradeLevel() const;
   int getPower() const;
   float getRange() const;
-  sf::CircleShape getRangeShape() const;
-  Tower::RateOfFire getRateOfFire() const;
   bool getIsFiring() const;
   Tower::SpecialEffect getSpecialEffect() const;
   virtual int getBuyCost() const = 0;
@@ -49,8 +45,6 @@ public:
   void setUpgradeLevel(Tower::UpgradeLevel _upgrade_level);
   void setPower(int _power);
   virtual void setRange(float _range) = 0;
-  void setRangeShape(float range);
-  void setRateOfFire(Tower::RateOfFire _rate_of_fire);
   void setIsFiring(bool b);
   void setSpecialEffect(Tower::SpecialEffect _special_effect);
   void setBuyCost(int _buy_cost);
@@ -60,7 +54,6 @@ public:
   void setStrategy(TowerStrategy* newStrategy);
 
   virtual std::string getTowerSpecs() = 0;
-  virtual bool circleToCircleIntersection(GameObject* game_object);
   void applySpecialEffect(Critter* critter);
 
 protected:
@@ -70,14 +63,10 @@ protected:
   UpgradeLevel _upgrade_level;
   int _power;
   float _range;
-  sf::CircleShape _range_shape;
-  RateOfFire _rate_of_fire;
   SpecialEffect _special_effect;
   int _buy_cost;
   int _upgrade_cost;
   int _sell_cost;
-  sf::Clock clock;
-  sf::Time time;
   Critter* _target;
   std::unique_ptr<TowerStrategy> _strategy;
 
